74e2ac0e280ae7d715ad8c2de889cf132a64e647
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-10 22:43:00 +0300

    Updated: Configure React router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
 import './App.css'
 
 const App = () => {
 
   return (
-    <>
-      <h1>init ts react project</h1>
-    </>
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
