af67c38f2cd6e9439d1b4ac239f5417b1e461ac9
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-12 13:15:55 +0300

    Fixing: Navbar with a Layout Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
 import "./server"
 import './App.css'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Layout from "./components/Layout"
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
