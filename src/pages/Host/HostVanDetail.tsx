d3cdce93dbdec7df48eb2ac357f755d380b56f38
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-12 21:39:56 +0300

    Updating: Host Vans && Host Van Details

--- src/pages/Host/HostVanDetail.tsx
+++ src/pages/Host/HostVanDetail.tsx
@@ -1,8 +1,47 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+
+interface Van {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+    hostId: number;
+    type: string;
+}
+
 const HostVanDetail = () => {
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = useState<Van | null>(null)
+
+    useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+            .then(res => res.json())
+            .then(data => setCurrentVan(data.vans[0]))
+    }, [id])
+
+    console.log(currentVan)
+    if (!currentVan) {
+        return <h1>Loading...</h1>
+    }
+
     return (
-        <div>
-            HostVanDetail
-        </div>
+        <section>
+            <div className="host-van-detail-layout-container">
+                <div className="host-van-detail">
+                    <img src={currentVan.imageUrl} />
+                    <div className="host-van-detail-info-text">
+                        <i
+                            className={`van-type van-type-${currentVan.type}`}
+                        >
+                            {currentVan.type}
+                        </i>
+                        <h3>{currentVan.name}</h3>
+                        <h4>${currentVan.price}/day</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
