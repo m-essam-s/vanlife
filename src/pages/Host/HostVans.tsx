7938cb3d329a52733165efd107dc1d3e19bf603c
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-19 11:51:44 +0300

    Updating: Fix remaining absolute paths

--- src/pages/Host/HostVans.tsx
+++ src/pages/Host/HostVans.tsx
@@ -19,7 +19,7 @@ const HostVans = () => {
     }, [])
     const hostVansEls = vans.map(van => (
         <Link
-            to={`/host/vans/${van.id}`}
+            to={`./${van.id}`}
             key={van.id}
             className="host-van-link-wrapper"
         >
--- src/pages/Vans/Vans.tsx
+++ src/pages/Vans/Vans.tsx
@@ -31,7 +31,7 @@ const Vans = () => {
     const vanElements = filteredVans.map(van => (
         <div key={van.id} className="van-tile">
             <Link
-                to={`/vans/${van.id}`}
+                to={`./${van.id}`}
                 aria-label={`View details for ${van.name}, priced at $${van.price} per day`}
             >
                 <img src={van.imageUrl} alt={`Image of ${van.name}`} />
