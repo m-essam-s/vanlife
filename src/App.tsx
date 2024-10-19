98c6e5bb412c54263d8860bc3c6b1bbd5ecb64e9
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-19 12:51:24 +0300

    Adding: 404 Page

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,8 @@ import HostVanInfo from "./pages/Host/HostVanInfo"
 import HostVanPricing from "./pages/Host/HostVanPricing"
 import HostVanPhotos from "./pages/Host/HostVanPhotos"
 
+import NotFound from "./pages/NotFound"
+
 const App: React.FC = () => {
   return (
     <BrowserRouter>
@@ -41,6 +43,7 @@ const App: React.FC = () => {
               <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
